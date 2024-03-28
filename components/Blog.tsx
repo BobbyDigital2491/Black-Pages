9e151e2c0bed67e53f1cd9b521b80b1de2c145df
Robbob24 <r.lawrence2491@gmail.com>
2024-03-28 08:18:59 -0400

    20th

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -12,7 +12,7 @@ const Blog = () => {
       <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <article className="flex max-w-xl flex-col items-start justify-between">
           <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime="2020-03-16" className="text-gray-500">Mar 16, 2020</time>
+            <time dateTime="2020-03-16" className="text-gray-500">Mar 16, 2024</time>
             <h1 className="relative z-10 rounded-full bg-green-400 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Tech</h1>
           </div>
           <div className="group relative">
@@ -22,7 +22,7 @@ const Blog = () => {
                 Creating the Black Pages
               </a>
             </h3>
-            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Using some of the latest and best practices in modern software development, this app is created  </p>
+            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Using some of the latest and best practices in modern software development, this app is created fro small and large businesses and entreprenuers  </p>
           </div>
           <div className="relative mt-8 flex items-center gap-x-4">
             <img src="https://viccci.xyz/log.png" alt="" className="h-14 w-10 rounded-full bg-gray-50"/>
@@ -40,7 +40,7 @@ const Blog = () => {
         </article>
         <article className="flex max-w-xl flex-col items-start justify-between">
           <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime="2020-03-16" className="text-gray-500">Mar 16, 2020</time>
+            <time dateTime="2020-03-16" className="text-gray-500">Mar 28, 2024</time>
             <h1 className="relative z-10 rounded-full bg-green-400 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Data Analytics</h1>
           </div>
           <div className="group relative">
@@ -67,7 +67,7 @@ const Blog = () => {
         </article>
         <article className="flex max-w-xl flex-col items-start justify-between">
           <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime="2020-03-16" className="text-gray-500">Mar 16, 2020</time>
+            <time dateTime="2020-03-16" className="text-gray-500">Mar 28, 2024</time>
             <h1 className="relative z-10 rounded-full bg-green-400 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</h1>
           </div>
           <div className="group relative">
